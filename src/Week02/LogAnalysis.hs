111746d07bf1defcc673e9c74d98efb611297865
Brad Bow <brad.bow@live.com>
2018-08-28 19:35:52 +1000

    w02 solutions

--- src/Week02/LogAnalysis.hs
+++ src/Week02/LogAnalysis.hs
@@ -18,19 +18,62 @@ import Week02.Log
   )
 
 parseMessage :: String -> LogMessage
-parseMessage = error "Week02.LogAnalysis#parseMessage not implemented"
+parseMessage s = case words s of
+  "E":l:t:r -> LogMessage (Error (read l)) (read t) (unwords r)
+  "I":t:r -> LogMessage Info (read t) (unwords r)
+  "W":t:r -> LogMessage Warning (read t) (unwords r)
+  ws -> Unknown (unwords ws)
 
 parse :: String -> [LogMessage]
-parse = error "Week02.LogAnalysis#parse not implemented"
+parse = map parseMessage . lines
+
+-- ************************************* Fancy ******************************************
+-- parse = map parseMessage . lines
 
 insert :: LogMessage -> MessageTree -> MessageTree
-insert = error "Week02.LogAnalysis#insert not implemented"
+insert (Unknown _) t = t
+insert x Leaf = Node Leaf x Leaf
+insert x@(LogMessage _ t1 _) (Node lt y@(LogMessage _ t2 _) rt)
+  | t1 < t2 = Node (insert x lt) y rt
+  | otherwise = Node lt y (insert x rt)
+insert _ _ = undefined
 
 build :: [LogMessage] -> MessageTree
-build = error "Week02.LogAnalysis#build not implemented"
+build xs =
+  let
+    accumulate ys tree = case ys of
+      [] -> tree
+      (z:zs) -> accumulate zs (insert z tree)
+  in
+    accumulate xs Leaf
+
+-- ************************************* Fancy ******************************************
+-- build = foldr insert Leaf
 
 inOrder :: MessageTree -> [LogMessage]
-inOrder = error "Week02.LogAnalysis#inOrder not implemented"
+inOrder Leaf = []
+inOrder (Node Leaf x rt) = x : inOrder rt
+inOrder (Node lt x rt) = inOrder lt ++ [x] ++ inOrder rt
 
 whatWentWrong :: [LogMessage] -> [String]
-whatWentWrong = error "Week02.LogAnalysis#whatWentWrong not implemented"
+whatWentWrong xs =
+  let
+    isSevere (LogMessage (Error n) _ _) = n >= 50
+    isSevere _ = False
+    accumulate [] = []
+    accumulate (Unknown _:ys) = accumulate ys
+    accumulate (y@(LogMessage _ _ m):ys) =
+      if isSevere y
+      then m : accumulate ys
+      else accumulate ys
+  in
+    accumulate (inOrder (build xs))
+
+-- ************************************* Fancy ******************************************
+-- whatWentWrong =
+--   let
+--     isSevere (LogMessage (Error n) _ _) = n >= 50
+--     isSevere _ = False
+--     getMsg (LogMessage _ _ m) = m
+--   in
+--     map getMsg . filter isSevere . inOrder . build
