356411a34b59ca076d67ab981567472e906be827
Brad Bow <brad.bow@live.com>
2018-08-28 19:36:04 +1000

    w03 solutions

--- src/Week03/Golf.hs
+++ src/Week03/Golf.hs
@@ -2,13 +2,31 @@ module Week03.Golf
   ( skips
   , localMaxima
   , histogram
+  , histogramGen
   ) where
 
 skips :: [a] -> [[a]]
-skips = error "Week03.Golf#skips not implemented"
+skips xs = map (`everyNth` xs) [1..(length xs)]
+  where everyNth n = map snd . filter ((==0) . (`mod` n) . fst) . zip [1..]
 
 localMaxima :: [Integer] -> [Integer]
-localMaxima = error "Week03.Golf#localMaxima not implemented"
+localMaxima = map (\(_, x, _) -> x) . filter (\(x, y, z) -> y > x && y > z) . triples
+  where
+    triples xs = zip3 xs (drop 1 xs) (drop 2 xs)
+
+-- general version that allows for any domain (between 0 and 9), not just 0..9
+-- e.g. putStr (histogramGen [0..3] [0,0,1,2,2,2,3])
+histogramGen :: [Integer] -> [Integer] -> String
+histogramGen domain xs =
+  let
+    freq n = length . filter (== n)
+    freqs = map (`freq` xs) domain
+    buildRow n = map (\f -> if f >= n then '*' else ' ') freqs
+    rows = map buildRow (reverse [1..(maximum freqs)])
+    separator = replicate (length domain) '='
+    legend = concatMap show domain
+  in
+    unlines (rows ++ [separator, legend])
 
 histogram :: [Integer] -> String
-histogram = error "Week03.Golf#histogram not implemented"
+histogram = histogramGen [0..9]
