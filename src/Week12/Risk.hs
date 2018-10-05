2350bda5e2649672b4792d5dd25777caf8246bdd
Brad Bow <brad.bow@stacktracehq.com>
2018-10-05 15:45:26 +1000

    week 12 solns

--- src/Week12/Risk.hs
+++ src/Week12/Risk.hs
@@ -39,20 +39,56 @@ data Battlefield = Battlefield
 ------------------------------------------------------------
 -- Exercise 2
 
+fightingArmySizes :: Battlefield -> (Army, Army)
+fightingArmySizes (Battlefield as ds) = (min 3 (as - 1), min 2 ds)
+
+genNDieRolls :: Int -> Rand StdGen [DieValue]
+genNDieRolls n = replicateM n die
+
+toPair :: Battlefield -> (Army, Army)
+toPair = attackers &&& defenders
+
+gameOver :: Battlefield -> Bool
+gameOver (Battlefield as ds) = as < 2 || ds == 0
+
+fight :: Battlefield -> ([DieValue], [DieValue]) -> Battlefield
+fight bf (aRolls, dRolls) = uncurry Battlefield outcome
+  where
+    outcome = foldr fightPair (toPair bf) zipped
+    sortDesc = reverse . sort
+    zipped = zip (sortDesc aRolls) (sortDesc dRolls)
+    fightPair p acc
+      | uncurry (>) p = (fst acc, snd acc - 1)
+      | otherwise = (fst acc - 1, snd acc)
+
 battle :: Battlefield -> Rand StdGen Battlefield
-battle = error "Week12.Risk#battle not implemented"
+battle bf
+  | gameOver bf = return bf
+  | otherwise = do
+      let (numAttackers, numDefenders) = fightingArmySizes bf
+      attackerRolls <- genNDieRolls numAttackers
+      defenderRolls <- genNDieRolls numDefenders
+      return (fight bf (attackerRolls, defenderRolls))
 
 ------------------------------------------------------------
 -- Exercise 3
 
 invade :: Battlefield -> Rand StdGen Battlefield
-invade = error "Week12.Risk#invade not implemented"
+invade bf = battle bf >>= continueIfPossible
+  where
+    continueIfPossible bf'
+      | gameOver bf' = return bf'
+      | otherwise = invade bf'
 
 ------------------------------------------------------------
 -- Exercise 4
 
+noDefendersLeft :: Battlefield -> Bool
+noDefendersLeft = (== 0) . defenders
+
 successProb :: Battlefield -> Rand StdGen Double
-successProb = error "Week12.Risk#successProb not implemented"
+successProb bf = replicateM 1000 (invade bf) >>= \outcomes ->
+  return (fromIntegral (length $ filter noDefendersLeft outcomes) / 1000)
 
 ------------------------------------------------------------
 -- Exercise 5
